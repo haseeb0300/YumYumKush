86d4c4c6311f9da2a736957d19dcc76593f617b9
usamahussain <usamahussain400@gmail.com>
2022-04-21 05:51:51 +0500

    fornt page and footer

--- src/app/App.js
+++ src/app/App.js
@@ -9,6 +9,8 @@ import store from '../store/store';
 import '../assets/styles/base/_style.scss'
 import '../assets/styles/base/_header.scss'
 import '../assets/styles/base/_dashboard.scss'
+import '../assets/styles/base/_footer.scss'
+
 
 
 
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -18,6 +18,14 @@ import TestimonialImg2 from '../../assets/images/dashboard/section6/2.png'
 import TestimonialImg3 from '../../assets/images/dashboard/section6/3.png'
 import slider1 from '../../assets/images/dashboard/section7/1.png'
 import Logo from '../../assets/images/header/Logo.svg'
+import envolpe from '../../assets/images/dashboard/section7/envolpe.png'
+
+import footer from '../../assets/images/footer/footer.png'
+import footerlogo from '../../assets/images/footer/logo.png'
+import socialMedia1 from '../../assets/images/footer/1.png'
+import socialMedia2 from '../../assets/images/footer/2.png'
+import socialMedia3 from '../../assets/images/footer/3.png'
+import socialMedia4 from '../../assets/images/footer/4.png'
 
 
 import sycrup from '../../assets/images/dashboard/section3/sycrup.svg'
@@ -420,70 +428,70 @@ class Dashboard extends Component {
                      <div className='col-md-7'>
                         <p className='text1 poppins_regular'><label className='horizontal-Line'></label>Testimonials</p>
                         <p className='heading poppins_bold'>WHAT OUR VALUABLE <label className='primarycolor'> CLIENTS</label> SAY ABOUT US..</p>
-      
+
                      </div>
-                   
+
                   </div>
                </div>
 
                <div className='col-md-12 mt-mb-30'>
                   <div className='row'>
                      <div className='col-md-6'>
-                      <div className='testimoninalCard'>
-                         <div className='col-12'>
-                            <div className='row'>
-                               <div className='col-4 my-auto text-center'>
-                                  <img src={TestimonialImg1}/>
+                        <div className='testimoninalCard'>
+                           <div className='col-12'>
+                              <div className='row'>
+                                 <div className='col-4 my-auto text-center'>
+                                    <img src={TestimonialImg1} />
 
-                               </div>
-                               <div className='col-8  my-auto'>
-                                  <p className='poppins_medium testimoninalCardText1'>Emma White</p>
-                                  <p className='poppins_medium testimoninalCardText2 '>Social Media Manager, Uber</p>
+                                 </div>
+                                 <div className='col-8  my-auto'>
+                                    <p className='poppins_medium testimoninalCardText1'>Emma White</p>
+                                    <p className='poppins_medium testimoninalCardText2 '>Social Media Manager, Uber</p>
 
-                               </div>
-                            </div>
-                         </div>
+                                 </div>
+                              </div>
+                           </div>
 
-                      </div>
-                      <div className='testimoninalCard'>
-                         <div className='col-12'>
-                            <div className='row'>
-                               <div className='col-4 my-auto text-center'>
-                                  <img src={TestimonialImg2}/>
+                        </div>
+                        <div className='testimoninalCard'>
+                           <div className='col-12'>
+                              <div className='row'>
+                                 <div className='col-4 my-auto text-center'>
+                                    <img src={TestimonialImg2} />
 
-                               </div>
-                               <div className='col-8  my-auto'>
-                                  <p className='poppins_medium testimoninalCardText1'>Danial Smith</p>
-                                  <p className='poppins_medium testimoninalCardText2 '>Developer, Foodpanda</p>
+                                 </div>
+                                 <div className='col-8  my-auto'>
+                                    <p className='poppins_medium testimoninalCardText1'>Danial Smith</p>
+                                    <p className='poppins_medium testimoninalCardText2 '>Developer, Foodpanda</p>
 
-                               </div>
-                            </div>
-                         </div>
+                                 </div>
+                              </div>
+                           </div>
 
-                      </div>
-                      <div className='testimoninalCard'>
-                         <div className='col-12'>
-                            <div className='row'>
-                               <div className='col-4 my-auto text-center'>
-                                  <img src={TestimonialImg3}/>
+                        </div>
+                        <div className='testimoninalCard'>
+                           <div className='col-12'>
+                              <div className='row'>
+                                 <div className='col-4 my-auto text-center'>
+                                    <img src={TestimonialImg3} />
 
-                               </div>
-                               <div className='col-8  my-auto'>
-                                  <p className='poppins_medium testimoninalCardText1'>Chris Brown</p>
-                                  <p className='poppins_medium testimoninalCardText2 '>Lead actor, Hollywood</p>
+                                 </div>
+                                 <div className='col-8  my-auto'>
+                                    <p className='poppins_medium testimoninalCardText1'>Chris Brown</p>
+                                    <p className='poppins_medium testimoninalCardText2 '>Lead actor, Hollywood</p>
 
-                               </div>
-                            </div>
-                         </div>
+                                 </div>
+                              </div>
+                           </div>
 
-                      </div>
+                        </div>
                      </div>
                      <div className='col-md-6 mt-3'>
                         <p className='poppins_semibold text3'>We build trust and transparency between customer and our company.</p>
                         <p className='poppins_light text3'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
 
                      </div>
-                   
+
                   </div>
                </div>
             </section>
@@ -496,62 +504,151 @@ class Dashboard extends Component {
                      <div className='col-md-7'>
                         <p className='text1 poppins_regular'><label className='horizontal-Line'></label>Reward System</p>
                         <p className='heading poppins_bold'>CUSTOMER   <label className='primarycolor'> REWARDS</label> PROGRAM.</p>
-      
+
                      </div>
-                   
+
                   </div>
                </div>
                <div className='col-md-12'>
-               <Carousel autoPlay infiniteLoop>
-                                    <div className='carouselCard'>
-                                       <img className='carouselImg' src={slider1}/>
-                                       <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
-                                  <p className='carouselText2 poppins_regular'>Read more</p>
-                                    </div>
-                                    <div className='carouselCard'>
-                                       <img className='carouselImg' src={slider1}/>
-                                       <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
-                                  <p className='carouselText2 poppins_regular'>Read more</p>
-                                    </div>
-                                    <div className='carouselCard'>
-                                       <img className='carouselImg' src={slider1}/>
-                                       <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
-                                  <p className='carouselText2 poppins_regular'>Read more</p>
-                                    </div>
-                                    <div className='carouselCard'>
-                                       <img className='carouselImg' src={slider1}/>
-                                       <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
-                                  <p className='carouselText2 poppins_regular'>Read more</p>
-                                    </div>
-                                   
-                                </Carousel>
+                  <Carousel autoPlay infiniteLoop>
+                     <div className='carouselCard'>
+                        <img className='carouselImg' src={slider1} />
+                        <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
+                        <p className='carouselText2 poppins_regular'>Read more</p>
+                     </div>
+                     <div className='carouselCard'>
+                        <img className='carouselImg' src={slider1} />
+                        <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
+                        <p className='carouselText2 poppins_regular'>Read more</p>
+                     </div>
+                     <div className='carouselCard'>
+                        <img className='carouselImg' src={slider1} />
+                        <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
+                        <p className='carouselText2 poppins_regular'>Read more</p>
+                     </div>
+                     <div className='carouselCard'>
+                        <img className='carouselImg' src={slider1} />
+                        <p className='poppins_bold carouselText '>Redeem 12 cards with your $25 order for your choice of one of the following</p>
+                        <p className='carouselText2 poppins_regular'>Read more</p>
+                     </div>
+
+                  </Carousel>
 
                </div>
 
-             
+
             </section>
             {/* section7 end */}
-             {/* section8 start */}
+            {/* section8 start */}
 
 
             <section className='container section8'>
                <div className='col-md-12 mt-mb-30 text-center'>
                   <div className='centerheading'>
                      <p className='text1 poppins_regular'><label className='horizontal-Line'></label> ABOUT US <label className='horizontal-Line'></label></p>
-                     <p className='heading poppins_bold'>LET US KNOW ABOUT US MORE   <label className='primarycolor'> CATEGORIES</label> </p>
+                     <p className='heading poppins_bold'>LET US KNOW ABOUT US MORE    </p>
                      <p className='text2 poppins_light'>If you live, work or are just passing through the 760 High Desert area, call Route 66 Dispensary. Route 66 is the premier dispensary for miles around.
-We strive to provide every customer with a great delivery experience, as well as pride ourselves in having a variety of THC and CBD products to choose from that can be delivered straight to you.
-Our products are handpicked by our highly knowledgeable cannabis curators and we also offer accessories that compliment our cannabis for your experience.
-We value integrity and quality in our products and are dedicated to providing the best quality cannabis products we can find for our consumer's medical needs and/or recreational experience.</p>
-                
-                  <img className='mt-5' src ={Logo}/>
+                        We strive to provide every customer with a great delivery experience, as well as pride ourselves in having a variety of THC and CBD products to choose from that can be delivered straight to you.
+                        Our products are handpicked by our highly knowledgeable cannabis curators and we also offer accessories that compliment our cannabis for your experience.
+                        We value integrity and quality in our products and are dedicated to providing the best quality cannabis products we can find for our consumer's medical needs and/or recreational experience.</p>
+
+                     <img className='mt-5' src={Logo} />
                   </div>
                </div>
-             
 
-          
+
+
 
             </section>
+            {/* section8 end */}
+            {/* section9 start */}
+
+
+            <section className='container section8'>
+               <div className='col-md-12 mt-mb-30 text-center'>
+                  <div className='centerheading'>
+                     <p className='text1 poppins_regular'><label className='horizontal-Line'></label> SUBSCRIBE NOW <label className='horizontal-Line'></label></p>
+                     <p className='heading poppins_bold'>Subscribe for latest news!  </p>
+                     <p className='text2 poppins_light'>WE HELP PEOPLE ELEVATE THEIR HAPPINESS.</p>
+                     <div className='col-md-12'>
+                        <img className='envopleImg' src={envolpe} />
+                        <input className='subscribeInput' placeholder='Enter email address'></input>
+                        <button className='subscribebtn'>SUBCRIBE NOW</button>
+                     </div>
+                  </div>
+               </div>
+
+
+
+
+            </section>
+            <footer>
+               <div className='col-md-12 p-0'>
+                  <img className='w-100 footerTop' src={footer} />
+                  <div className='col-md-12 footerMid p-0'>
+
+                     <div className='footerMid'>
+                        <div className='col-md-12 pb-5'>
+                           <div className='row'>
+                              <div className='col-md-5 text-center'>
+                                 <img src={footerlogo} />
+                                 <p className='footerText1 poppins_regular'>support@yumyumkush.com</p>
+                                 <button className='socialMediaContainer'>
+                                    <img src={socialMedia1} />
+                                 </button>
+                                 <button className='socialMediaContainer'>
+                                    <img src={socialMedia2} />
+
+                                 </button>
+                                 <button className='socialMediaContainer'>
+                                    <img src={socialMedia3} />
+
+                                 </button>
+                                 <button className='socialMediaContainer'>
+                                    <img src={socialMedia4} />
+
+                                 </button>
+
+                              </div>
+                              <div className='col-md-2'></div>
+                              <div className='col-md-5'>
+                                 <div className='row'>
+                                    <div className='col-md-6'>
+                                       <p className='poppins_semibold footerNav'>Navigation</p>
+                                       <p className='poppins_regular footerNavItem'>Home</p>
+                                       <p className='poppins_regular footerNavItem'>Store</p>
+                                       <p className='poppins_regular footerNavItem'>Contact us</p>
+                                       <p className='poppins_regular footerNavItem'>About us</p>
+                                       <p className='poppins_regular footerNavItem'>Customer Feedback</p>
+                                      
+
+                                    </div>
+                                    <div className='col-md-6'>
+                                    <p className='poppins_semibold footerNav'>Extra</p>
+                                       <p className='poppins_regular footerNavItem'>Explore</p>
+                                       <p className='poppins_regular footerNavItem'>Liquid</p>
+                                       <p className='poppins_regular footerNavItem'>Weed</p>
+                                       <p className='poppins_regular footerNavItem'>Medication</p>
+                                       <p className='poppins_regular footerNavItem'>Products</p>
+                                    </div>
+                                 </div>
+
+
+                              </div>
+                           </div>
+                        
+                        </div>
+                        <div className='col-md-12 text-center'>
+                           <div className='footerLine'></div>
+                              <p className='poppins_regular footerCopyRight mb-0'>© NextisHost 2022 | All right reserves Yum Yum Kush 2022</p>
+
+                           </div>
+
+                     </div>
+
+                  </div>
+               </div>
+            </footer>
             {/* section8 end */}
 
          </>
